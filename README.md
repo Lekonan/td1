aa1b96dfa72cec88a1109ada611aeb14978c5333
Lekonan <davidonesime@gmail.com>
2022-11-26 21:22:58 +0100

    Fin de q2 partie 5

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## TD SRIO 
+
+## Partie 5
+
+- On commence par créer les niveaux(groupes sur le système)
+- On crée les dossiers et on les ajoute dans leur niveau respectifs
+- 
---
+++ partie5_q2.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Recuperation des parametres
+#NIVEAU 1
+usersNiveauUn=$(cat organisation.json | jq -r '.niveau_1.utilisateurs[] | .nom' )
+droitUsersNiveauUn=$(cat organisation.json | jq -r '.niveau_1.utilisateurs[] | .droit' )
+dossiersNiveauUn=$(cat organisation.json | jq -r '.niveau_1.dossiers[] | .' )
+
+#NIVEAU 2
+usersNiveauDeux=$(cat organisation.json | jq -r '.niveau_2.utilisateurs[] | .nom' )
+droitUsersNiveauDeux=$(cat organisation.json | jq -r '.niveau_2.utilisateurs[] | .droit' )
+dossiersNiveauDeux=$(cat organisation.json | jq -r '.niveau_2.dossiers[] | .' )
+
+#NIVEAU 3
+usersNiveauTrois=$(cat organisation.json | jq -r '.niveau_3.utilisateurs[] | .nom' )
+droitUsersNiveauTrois=$(cat organisation.json | jq -r '.niveau_3.utilisateurs[] | .droit' )
+dossiersNiveauTrois=$(cat organisation.json | jq -r '.niveau_3.dossiers[] | .' )
+
+#CREATION DE GROUPES SUR LE SYSTEME
+sudo groupadd niveau_1
+sudo groupadd niveau_2
+sudo groupadd niveau_3
+
+#CREATION ET AJOUT DES DOSSIERS DANS LES GROUPES RESPECTIFS
+#NIVEAU 1
+for dossier in $dossiersNiveauUn
+do
+    sudo mkdir /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+    sudo chown root:niveau_1 /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+    sudo chmod -R 700 /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+done
+
+#NIVEAU 2
+for dossier in $dossiersNiveauDeux
+do
+    sudo mkdir /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+    sudo chown root:niveau_2 /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+    sudo chmod -R 700 /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+done
+
+#NIVEAU 3
+for dossier in $dossiersNiveauTrois
+do
+    sudo mkdir /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+    sudo chown root:niveau_3 /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+    sudo chmod -R 700 /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+done
+
+
+#GESTION DES UTILISATEURS DU NIVEAU 1 : CREATION, AJOUT AU GROUPE, AJOUT DES DROITS D'UN UTILISATEUR SUR UN DOSSIER
+for user in $usersNiveauUn
+do
+    #Les droit d'un utilisateur
+    droit=$(cat organisation.json | jq --arg u "$user" -r '.niveau_1.utilisateurs[]| select(.nom==$u) | .droit')
+    #Creation de l'utilisateur, ajout au groupe
+    sudo useradd -g niveau_1 -s /bin/bash $user
+    #Ajout des droits d'un utilisateur sur un dossier avec des ACL
+    for dossier in $dossiersNiveauUn
+    do
+        sudo setfacl -m u:$user:$droit /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+    done
+done
+
+#GESTION DES UTILISATEURS DU NIVEAU 2 : CREATION, AJOUT AU GROUPE, AJOUT DES DROITS D'UN UTILISATEUR SUR UN DOSSIER
+for user in $usersNiveauDeux
+do
+    #Les droit d'un utilisateur
+    droit=$(cat organisation.json | jq --arg u "$user" -r '.niveau_2.utilisateurs[]| select(.nom==$u) | .droit')
+    #Creation de l'utilisateur, ajout au groupe
+    sudo useradd -g niveau_2 -s /bin/bash $user
+    #Ajout des droits d'un utilisateur sur un dossier avec des ACL
+    for dossier in $dossiersNiveauDeux
+    do
+        sudo setfacl -m u:$user:$droit /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+    done
+    #DROIT SUR LE NIVEAU INFERIEUR (NIVEAU 1)
+    for dossier in $dossiersNiveauUn
+    do
+        sudo setfacl -m u:$user:$droit /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+    done
+done
+
+
+#GESTION DES UTILISATEURS DU NIVEAU 3 : CREATION, AJOUT AU GROUPE, AJOUT DES DROITS D'UN UTILISATEUR SUR UN DOSSIER
+for user in $usersNiveauTrois
+do
+    #Les droit d'un utilisateur
+    droit=$(cat organisation.json | jq --arg u "$user" -r '.niveau_3.utilisateurs[]| select(.nom==$u) | .droit')
+    #Creation de l'utilisateur, ajout au groupe
+    sudo useradd -g niveau_3 -s /bin/bash $user
+    #Ajout des droits d'un utilisateur sur un dossier avec des ACL
+    for dossier in $dossiersNiveauTrois
+    do
+        sudo setfacl -m u:$user:$droit /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+    done
+    #CONTROLE SUR LE NIVEAU 2
+    for dossier in $dossiersNiveauDeux
+    do
+        sudo setfacl -m u:$user:$droit /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+    done
+    #CONTROLE SUR LE NIVEAU 1
+    for dossier in $dossiersNiveauUn
+    do
+        sudo setfacl -m u:$user:$droit /home/orphee/ESIR/semestre_1/srio/td-srio/$dossier
+    done
+done
+
